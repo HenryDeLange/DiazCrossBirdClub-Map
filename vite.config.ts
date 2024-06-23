1b9a90a821f706ffff9cd569988c629aca336a07
Henry <36891536+HenryDeLange@users.noreply.github.com>
2024-06-23 12:22:29 +0200

    github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(({ mode }) => ({
-    base: 'DiazCrossBirdClub-Map',
+    base: '',
     test: {
         css: false,
         include: ['src/**/__tests__/*'],
